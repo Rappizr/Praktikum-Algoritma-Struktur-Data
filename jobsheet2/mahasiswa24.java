da40c22fc5eb919b5d383662179afdf0c5ba389c
Rappizr <mukhammadraffi5@gmail.com>
2026-02-21 21:51:21 +0700

    latihan 1

---
+++ jobsheet2/mahasiswa24.java
@@ -0,0 +1,34 @@
+package jobsheet2;
+
+public class mahasiswa24 {
+
+  String nama, nim, kelas;
+        double ipk;
+    void tampilkanInformasi () {
+    System.out.println("NAMA: "+nama);
+    System.out.println("NIM: "+nim);
+    System.out.println("IPK: "+ipk);
+    System.out.println("KELAS: "+kelas);
+    }
+    void ubahKelas(String kelasBaru){
+        kelas = kelasBaru;
+    }
+    void updateIpk(double ipkBaru){
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        }else{
+            System.out.println("IPK tidak valid harus rentan antara 0.0 sampai 4.0");
+        }
+    }
+    String nilaiKinerja(){
+        if(ipk>=3.5){
+            return "kinerja sangat baik";
+        }else if (ipk>=3.0) {
+            return  "kinerja baik";
+        }else if (ipk>=2.5) {
+            return "kinerja cukup";
+        }else{
+            return "kinerja kurang";
+        }
+    }
+}
