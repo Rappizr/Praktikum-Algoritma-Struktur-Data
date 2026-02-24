c252092d7b30dac73f8ce198bd2637d20d92d40c
Rappizr <mukhammadraffi5@gmail.com>
2026-02-24 12:43:13 +0700

    latihan 4

---
+++ jobsheet3/matakuliah24.java
@@ -0,0 +1,15 @@
+package jobsheet3;
+
+public class matakuliah24 {
+    public String kode;
+    public String nama;
+    public int sks;
+    public int jumlahJam;
+
+    public matakuliah24(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
