0449395055e5e5cd2843783eebac7010bfa19659
Rappizr <mukhammadraffi5@gmail.com>
2026-02-24 13:54:01 +0700

    modifikasi pertanyaan latihan 4

--- jobsheet3/matakuliah24.java
+++ jobsheet3/matakuliah24.java
@@ -1,15 +1,41 @@
 package jobsheet3;
 
+import java.util.Scanner;
+
 public class matakuliah24 {
     public String kode;
     public String nama;
     public int sks;
     public int jumlahJam;
 
+    public matakuliah24(){
+
+    }
+
     public matakuliah24(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
         this.nama = nama;
         this.sks = sks;
         this.jumlahJam = jumlahJam;
+    }
+
+    public void tambahdata(Scanner sc){
+        System.out.print("Kode : ");
+        this.kode = sc.nextLine();
+        System.out.print("Nama : ");
+        this.nama = sc.nextLine();
+        System.out.print("Sks : ");
+        this.sks = sc.nextInt();
+        System.out.print("Jumlah jam : ");
+        this.jumlahJam = sc.nextInt();
+        System.out.println("----------------------");
+    }
+
+    public void cetakinfo(){
+        System.out.println("Kode       : " + this.kode);
+        System.out.println("Nama       : " + this.nama);
+        System.out.println("Sks        : " + this.sks);
+        System.out.println("Jumlah jam : " + this.jumlahJam);
+        System.out.println("-------------------------------");
     }
 }
--- jobsheet3/matakuliahdemo24.java
+++ jobsheet3/matakuliahdemo24.java
@@ -1,29 +1,29 @@
 package jobsheet3;
 import java.util.Scanner;
-import jobsheet2.mataKuliah24;
 
 public class matakuliahdemo24 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        mataKuliah24[] arrayOfMatakuliah = new mataKuliah24[3];
-        String kode, nama, dummy;
-        int sks, jumlahJam;
+        matakuliah24[] arrayOfMatakuliah = new matakuliah24[3];
 
-        for (int i = 0; i < 3; i++) {
+        System.out.print("Masukkan jumlah matkul : ");
+        int jml = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < jml; i++) {
             System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
-            System.out.print("Kode       : ");
-            kode = sc.nextLine();
-            System.out.print("Nama       : ");
-            nama = sc.nextLine();
-            System.out.print("Sks        : ");
-            dummy = sc.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam : ");
-            dummy = sc.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("---------------------------------------");
-
-            arrayOfMatakuliah[i] = new mataKuliah24(kode, nama, sks, jumlahJam);
+            
+            arrayOfMatakuliah[i] = new matakuliah24(); 
+            
+            arrayOfMatakuliah[i].tambahdata(sc);
+            sc.nextLine(); 
         }
+
+        System.out.println("\n========== DAFTAR MATAKULIAH ==========");
+        for (int i = 0; i < jml; i++) {
+            System.out.println("Data matakuliah ke-" + (i + 1));
+            arrayOfMatakuliah[i].cetakinfo();
+        }
+        sc.close();
     }
 }
